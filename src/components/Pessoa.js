d791b2abae3bd73fceb5b49a6147156a7d174ea8
Marco <marcoantonioantonio949@gmail.com>
2024-10-04 10:11:20 -0300

    aula 6

--- src/components/Pessoa.js
+++ src/components/Pessoa.js
@@ -1,11 +1,15 @@
+import styles from "./Pessoa.module.css"
+
 function Pessoa({nome, idade, profissao, foto}) {
     return (
-        <div>
-            <img src={foto} alt={nome}/>
-            <h4>Nome: {nome}</h4>
-            <p>Idade: {idade}</p>
-            <p>Profissão: {profissao}</p>
-        </div>
+        <div className={styles.PessoaTamanho}>
+            <div className={styles.PessoaDiv}>
+                <img src={foto} alt={nome}/>
+                <h4 className={styles.PessoaH4}>Nome: {nome}</h4>
+                <p className={styles.PessoaP}>Idade: {idade}</p>
+                <p className={styles.PessoaP}>Profissão: {profissao}</p>
+            </div>
+        </div>   
     )
 }
 export default Pessoa
